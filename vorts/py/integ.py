86861cb5812c9aa44fa31a633975c738d4b7506c
zmoon92 <zmoon92@gmail.com>
2021-02-24 22:31:32 -0500

    Add model fn to create ds from integ output
    As opposed to creating the full empty hist before starting integ.
    This way, shouldn't need to have 2x memory.

--- vorts/py/integ.py
+++ vorts/py/integ.py
@@ -145,8 +145,10 @@ def integrate_manual(
         iter_l = tqdm_notebook(iter_l)
 
     # pre-allocate return arrays
-    xhist = np.empty((nv, nt))
+    xhist = np.empty((nv, nt+1))
     yhist = np.empty_like(xhist)
+    xhist[:,0] = x0
+    yhist[:,0] = y0
 
     # iterate over time index `l`
     x_lm1 = x0  # l-1 starts at 0
@@ -181,13 +183,13 @@ def integrate_manual(
             # step
             x_l, y_l = stepper(G, x_lm1, y_lm1, dt=dt0)
 
+        # store
+        xhist[:, l] = x_l
+        yhist[:, l] = y_l
+
         # new lm1 terms
         x_lm1 = x_l
         y_lm1 = y_l
-
-        # store
-        xhist[:, l-1] = x_lm1
-        yhist[:, l-1] = y_lm1
 
     return xhist, yhist
 
