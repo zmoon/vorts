d416bd5f4b9e8197c83bb7754ce58aa5fd193e73
zmoon92 <zmoon92@gmail.com>
2021-03-01 14:48:10 -0500

    jl: add Plots to compile.jl and add more notes

--- vorts/jl/compile.jl
+++ vorts/jl/compile.jl
@@ -1,13 +1,18 @@
-# Create a Julia sysimage for faster startup when calling from Py
-# Can take a few minutes and be 100s of MB
-
-using Pkg; Pkg.activate("./")
+# Create a Julia sysimage for faster startup
+#
+# The env should be activated when runinng:
+# julia --project=. compile.jl
+#
+# Note that it can take a few minutes and the sysimage can be 100s of MB
+#
+# To try it out:
+# julia --sysimage sys_vorts.so --project=. -e 'include(\"vorts.jl\"); main()'
 
 using PackageCompiler
 
 
 create_sysimage(
-    [:DifferentialEquations],#, :Plots],
+    [:DifferentialEquations, :Plots],
     sysimage_path="sys_vorts.so",
     precompile_execution_file="vorts.jl",
 )
