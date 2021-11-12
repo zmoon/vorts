2c8ea4a94993bbc07815f481b611ac60e1d314a9
zmoon <zmoon92@gmail.com>
2021-11-12 11:16:48 -0700

    Start simpler interface
    and use numpy.ctypeslib

--- vorts/f/api.py
+++ vorts/f/api.py
@@ -1,48 +1,61 @@
 import ctypes as ct
 import numpy as np
 
-lib = np.ctypeslib.load_library("libvortsf", "./_build")
-f = lib.asdf
+_lib = np.ctypeslib.load_library("libvortsf", "./_build")
 
-f.argtypes = [
+_run = _lib.asdf
+_run.argtypes = [
     ct.c_int,
-    ct.POINTER(ct.c_double),    
-    ct.POINTER(ct.c_double),    
-    ct.POINTER(ct.c_double),
+    np.ctypeslib.ndpointer(dtype=np.float64, ndim=1),
+    np.ctypeslib.ndpointer(dtype=np.float64, ndim=1),
+    np.ctypeslib.ndpointer(dtype=np.float64, ndim=1),
     ct.c_double,
     ct.c_int,
     ct.c_int,
-    ct.POINTER(ct.c_double),
-    ct.POINTER(ct.c_double),
+    np.ctypeslib.ndpointer(dtype=np.float64, ndim=2, flags="F, W"),
+    np.ctypeslib.ndpointer(dtype=np.float64, ndim=2, flags="F, W"),
 ]
 
-nv = 3
-x = np.array([-0.5, 0, 0.5], order="F")
-y = np.array([-0.3, 0.8, 0.3], order="F")
-G = np.full((nv,), 1., order="F")
-
-dt = 0.1
-nt = 5
-imethod = 2
-
-nout = nt + 1
-xout = np.full((nv, nout,), 0., order="F")
-yout = np.full((nv, nout,), 0., order="F")
+# Must match order in `int_scheme_from_index`!
+_methods = ["FT", "RK4"]
 
 
-f(
-    ct.c_int(nv),
-    x.ctypes.data_as(ct.POINTER(ct.c_double)), 
-    y.ctypes.data_as(ct.POINTER(ct.c_double)),
-    G.ctypes.data_as(ct.POINTER(ct.c_double)),
-    ct.c_double(dt),
-    ct.c_int(nt),
-    ct.c_int(imethod),
-    xout.ctypes.data_as(ct.POINTER(ct.c_double)), 
-    yout.ctypes.data_as(ct.POINTER(ct.c_double)), 
-)
+def run(G, x, y, *, dt, nt, method="RK4"):
 
-print("From Python: output")
-print(xout)
-print(yout)
+    nv = G.size
+    assert nv == x.size == y.size
 
+    imethod = _methods.index(method) + 1
+
+    # Allocate outputs
+    xout = np.full((nv, nt+1,), 0., order="F")
+    yout = np.full((nv, nt+1,), 0., order="F")
+
+    _run(
+        nv,
+        x,
+        y,
+        G,
+        dt,
+        nt,
+        imethod,
+        xout,
+        yout,
+    )
+
+    return xout, yout
+
+
+if __name__ == "__main__":
+
+    x = np.array([-0.5, 0, 0.5])
+    y = np.array([-0.3, 0.8, 0.3])
+    G = np.ones_like(x)
+    dt = 0.1
+    nt = 5
+
+    xout, yout = run(G, x, y, dt=dt, nt=nt)
+
+    print("From Python: output")
+    print(xout)
+    print(yout)
